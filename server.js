b9bf78c2f5d246bf405b5d2a2981393767c1b894
S.Jaya Abirami <156350178+jayaabi@users.noreply.github.com>
2026-02-04 08:20:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cloudinary = require('cloudinary');
+const app = require('./backend/app');
+const connectDatabase = require('./backend/config/database');
+const PORT = process.env.PORT || 4000;
+
+// UncaughtException Error
+process.on('uncaughtException', (err) => {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+});
+
+connectDatabase();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is Running! 🚀');
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => {
+        process.exit(1);
+    });
+});
